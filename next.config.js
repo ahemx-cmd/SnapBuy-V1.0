5f305feefd35f1173cbfe7593a7940b65781058c
Ahmed Musa <snapbuy367@gmail.com>
2025-07-20 15:59:43 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**', // Allow all image domains
-      },
-    ],
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: '**', // Allow all image domains
+    }],
   },
-  experimental: {
-    serverActions: true,
-  },
-};
+  // Remove the entire "experimental" section
+}
